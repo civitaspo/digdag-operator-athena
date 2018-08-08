074dac02791f961124095f533f5a832617cc350f
Civitaspo <civitaspo@gmail.com>
2018-08-09 03:45:02 +0900

    Change method permission

--- src/main/scala/pro/civitaspo/digdag/plugin/athena/operator/AthenaQueryOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/athena/operator/AthenaQueryOperator.scala
@@ -11,12 +11,12 @@ import scala.util.Try
 class AthenaQueryOperator(operatorName: String, context: OperatorContext, systemConfig: Config, templateEngine: TemplateEngine)
   extends AbstractAthenaOperator(operatorName, context, systemConfig, templateEngine) {
 
-  val queryOrFile: String = params.get("_command", classOf[String])
-  val tokenPrefix: String = params.get("token_prefix", classOf[String], "digdag-athena")
-  val database: Optional[String] = params.getOptional("database", classOf[String])
-  val output: String = params.get("output", classOf[String])
+  protected val queryOrFile: String = params.get("_command", classOf[String])
+  protected val tokenPrefix: String = params.get("token_prefix", classOf[String], "digdag-athena")
+  protected val database: Optional[String] = params.getOptional("database", classOf[String])
+  protected val output: String = params.get("output", classOf[String])
 
-  lazy val query: String = {
+  protected lazy val query: String = {
     val t = Try {
       val f = workspace.getFile(queryOrFile)
       workspace.templateFile(templateEngine, f.getPath, UTF_8, params)
