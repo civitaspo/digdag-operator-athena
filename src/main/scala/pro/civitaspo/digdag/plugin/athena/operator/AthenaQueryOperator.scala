dd654746259391aa6604155d98ca91a0354caacf
Civitaspo <civitaspo@gmail.com>
2018-08-09 03:43:23 +0900

    Write queryOrFile resolver

--- src/main/scala/pro/civitaspo/digdag/plugin/athena/operator/AthenaQueryOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/athena/operator/AthenaQueryOperator.scala
@@ -1,8 +1,12 @@
 package pro.civitaspo.digdag.plugin.athena.operator
 
+import java.nio.charset.StandardCharsets.UTF_8
+
 import com.google.common.base.Optional
 import io.digdag.client.config.Config
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
+
+import scala.util.Try
 
 class AthenaQueryOperator(operatorName: String, context: OperatorContext, systemConfig: Config, templateEngine: TemplateEngine)
   extends AbstractAthenaOperator(operatorName, context, systemConfig, templateEngine) {
@@ -11,6 +15,14 @@ class AthenaQueryOperator(operatorName: String, context: OperatorContext, system
   val tokenPrefix: String = params.get("token_prefix", classOf[String], "digdag-athena")
   val database: Optional[String] = params.getOptional("database", classOf[String])
   val output: String = params.get("output", classOf[String])
+
+  lazy val query: String = {
+    val t = Try {
+      val f = workspace.getFile(queryOrFile)
+      workspace.templateFile(templateEngine, f.getPath, UTF_8, params)
+    }
+    t.getOrElse(queryOrFile)
+  }
 
   override def runTask(): TaskResult = null
 }
