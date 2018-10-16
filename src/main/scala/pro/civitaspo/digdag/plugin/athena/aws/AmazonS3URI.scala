2540e9e9d77f6c8c675317205e1a83afd5c1372f
Civitaspo <civitaspo@gmail.com>
2018-10-17 08:41:15 +0900

    Stop to refactor

--- src/main/scala/pro/civitaspo/digdag/plugin/athena/aws/AmazonS3URI.scala
+++
@@ -1,7 +0,0 @@
-package pro.civitaspo.digdag.plugin.athena.aws
-
-import com.amazonaws.services.s3.AmazonS3URI
-
-object AmazonS3URI {
-  def apply(path: String): AmazonS3URI = new AmazonS3URI(path, false)
-}
