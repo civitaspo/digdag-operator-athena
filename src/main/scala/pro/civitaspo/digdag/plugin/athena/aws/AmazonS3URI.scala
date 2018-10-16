5dbb8887d6a1ee084ed5bbc80752b0818bec3a6c
Civitaspo <civitaspo@gmail.com>
2018-10-16 22:10:12 +0900

    Add AmazonS3URI under aws package

---
+++ src/main/scala/pro/civitaspo/digdag/plugin/athena/aws/AmazonS3URI.scala
@@ -0,0 +1,7 @@
+package pro.civitaspo.digdag.plugin.athena.aws
+
+import com.amazonaws.services.s3.AmazonS3URI
+
+object AmazonS3URI {
+  def apply(path: String): AmazonS3URI = new AmazonS3URI(path, false)
+}
