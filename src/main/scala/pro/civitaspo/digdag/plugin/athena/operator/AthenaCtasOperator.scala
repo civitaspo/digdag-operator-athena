6f5d91a8aab82a8b79685237ff1e7e5ba158c81a
Civitaspo <civitaspo@gmail.com>
2018-10-17 23:30:34 +0900

    Remove output parameter implementations

--- src/main/scala/pro/civitaspo/digdag/plugin/athena/operator/AthenaCtasOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/athena/operator/AthenaCtasOperator.scala
@@ -3,7 +3,6 @@ package pro.civitaspo.digdag.plugin.athena.operator
 import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
 
-import com.amazonaws.services.athena.model.{QueryExecution, QueryExecutionState}
 import com.amazonaws.services.s3.AmazonS3URI
 import com.google.common.base.Optional
 import io.digdag.client.config.{Config, ConfigException}
@@ -53,39 +52,6 @@ class AthenaCtasOperator(operatorName: String, context: OperatorContext, systemC
         case "overwrite" => Overwrite
         case unknown => throw new ConfigException(s"[$operatorName] query_mode '$unknown' is unsupported.")
       }
-    }
-  }
-
-  case class LastCtasQuery(
-    id: String,
-    database: Option[String] = None,
-    query: String,
-    outputCsvUri: AmazonS3URI,
-    outputCsvMetadataUri: AmazonS3URI,
-    scanBytes: Option[Long] = None,
-    execMillis: Option[Long] = None,
-    state: QueryExecutionState,
-    stateChangeReason: Option[String] = None,
-    submittedAt: Option[Long] = None,
-    completedAt: Option[Long] = None
-  )
-
-  object LastCtasQuery {
-
-    def apply(qe: QueryExecution): LastCtasQuery = {
-      new LastCtasQuery(
-        id = qe.getQueryExecutionId,
-        database = Try(Option(qe.getQueryExecutionContext.getDatabase)).getOrElse(None),
-        query = qe.getQuery,
-        outputCsvUri = AmazonS3URI(qe.getResultConfiguration.getOutputLocation),
-        outputCsvMetadataUri = AmazonS3URI(s"${qe.getResultConfiguration.getOutputLocation}.metadata"),
-        scanBytes = Try(Option(qe.getStatistics.getDataScannedInBytes.toLong)).getOrElse(None),
-        execMillis = Try(Option(qe.getStatistics.getEngineExecutionTimeInMillis.toLong)).getOrElse(None),
-        state = QueryExecutionState.fromValue(qe.getStatus.getState),
-        stateChangeReason = Try(Option(qe.getStatus.getStateChangeReason)).getOrElse(None),
-        submittedAt = Try(Option(qe.getStatus.getSubmissionDateTime.getTime / 1000)).getOrElse(None),
-        completedAt = Try(Option(qe.getStatus.getCompletionDateTime.getTime / 1000)).getOrElse(None)
-      )
     }
   }
 
