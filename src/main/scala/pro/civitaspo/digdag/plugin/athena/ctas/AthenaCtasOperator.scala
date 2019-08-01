387e79979578001008f9701632ab5a80e882382a
Civitaspo <civitaspo@gmail.com>
2019-08-02 01:46:23 +0900

    Delete the specified table and location, not the table location that the data catalog has when using `save_mode: overwrite` in `athena.ctas>`operator.

--- src/main/scala/pro/civitaspo/digdag/plugin/athena/ctas/AthenaCtasOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/athena/ctas/AthenaCtasOperator.scala
@@ -151,13 +151,20 @@ class AthenaCtasOperator(operatorName: String,
                     return TaskResult.empty(request)
                 }
 
+            case SaveMode.Overwrite =>
+                if (aws.glue.table.exists(catalogId, database, table)) {
+                    logger.info(s"'$database.$table' already exists, so delete this.")
+                    aws.glue.table.delete(catalogId, database, table)
+                }
+                if (location.exists(aws.s3.hasObjects)) {
+                    logger.info(s"${location.get} already exists, so delete this.")
+                    aws.s3.rm_r(location.get).foreach(uri => logger.info(s"Deleted: ${uri.toString}"))
+                }
+
             case _ => // do nothing
         }
 
         val subTask: Config = cf.create()
-        if (saveMode.equals(SaveMode.Overwrite)) {
-            subTask.setNested("+drop-before-ctas", buildDropTableSubTaskConfig(with_location = true))
-        }
         subTask.setNested("+ctas", buildCtasQuerySubTaskConfig())
         if (tableMode.equals(TableMode.DataOnly)) {
             subTask.setNested("+drop-after-ctas", buildDropTableSubTaskConfig(with_location = false))
